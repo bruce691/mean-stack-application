93339afc8bbc635d30e9af722e0ea0fd891ab3dd
nataliehall179 <nataliehall179@gmail.com>
2020-01-13 22:03:58 +0530

    added code to connect to local mongodb database

---
+++ webapi/db.js
@@ -0,0 +1,20 @@
+/**
+ * for mongodb connection
+ */
+const mongoose = require("mongoose");
+const conString = "mongodb://localhost:27017/customerDb";
+const conString =
+  "mongodb+srv://admin:Admin678@cluster0-iqrdy.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(
+  conString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (!err) console.log("Successfully connect to MondoDB...");
+    else
+      console.log(
+        "Connection to MongoDb failed :" + JSON.stringify(err, undefined, 2)
+      );
+  }
+);
+
+module.exports = mongoose;
