251a6d0372510b7f468ef25929bccd9a9fd5aca8
nataliehall179 <nataliehall179@gmail.com>
2020-01-15 19:30:14 +0530

    added cors package

--- webapi/index.js
+++ webapi/index.js
@@ -1,16 +1,18 @@
 /**
  * for express web api server
  */
-const bodyParser = require("body-parser");
-const { mongoose } = require("./db");
-const customer = require("./Controller/CustomerController");
+const bodyParser = require('body-parser');
+const { mongoose } = require('./db');
+const customer = require('./Controller/CustomerController');
+const cors = require('cors');
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
 //added middleware code
+app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.json());
-app.use("/customers", customer);
+app.use('/customers', customer);
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
