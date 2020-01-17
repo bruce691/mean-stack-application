23edbcf0b8db9eefb7fc29332b4410d84b846701
nataliehall179 <nataliehall179@gmail.com>
2020-01-17 22:11:59 +0530

    added code to fetch customer by id

--- client/src/app/customer.service.ts
+++ client/src/app/customer.service.ts
@@ -20,6 +20,10 @@ export class CustomerService {
     return this.http.get(this.APIUrl);
   }
 
+  GetCustomerById(id: string) {
+    return this.http.get(this.APIUrl + `/${id}`);
+  }
+
   PutCustomer(cust: Customer) {
     return this.http.put(this.APIUrl + `/${cust._id}`, cust);
   }
