797f17dd5ae764c6ebffe17d3af988d4642ef2c1
nataliehall179 <nataliehall179@gmail.com>
2020-01-20 18:00:36 +0530

    added env variable

--- webapi/db.js
+++ webapi/db.js
@@ -1,17 +1,20 @@
 /**
  * for mongodb connection
  */
-const mongoose = require("mongoose");
-const conString = "mongodb://localhost:27017/customerDb";
+const mongoose = require('mongoose');
+require('dotenv').config();
+const conString = process.env.DB_CONNECTION;
+console.log(conString);
+// const conString = "mongodb://localhost:27017/customerDb";
 // const conString =  "mongodb+srv://admin:Admin678@cluster0-iqrdy.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(
   conString,
   { useNewUrlParser: true, useUnifiedTopology: true },
   err => {
-    if (!err) console.log("Successfully connect to MondoDB...");
+    if (!err) console.log('Successfully connect to MondoDB...');
     else
       console.log(
-        "Connection to MongoDb failed :" + JSON.stringify(err, undefined, 2)
+        'Connection to MongoDb failed :' + JSON.stringify(err, undefined, 2)
       );
   }
 );
